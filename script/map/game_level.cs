86695a44d1c06e5546fec8f92b1a1dcd0912bc0e
Lukas Grünitz <l.gruenitz@fm-control.de>
2023-12-01 12:52:43 +0100

    Test

---
+++ script/map/game_level.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class game_level : Node2D
+{
+	private Button _pauseButton;
+	private Button _exitButton;
+
+	public override void _Ready()
+	{
+		_pauseButton=GetNode<Button>("PauseButton");
+		_exitButton=GetNode<Button>("ExitButton");
+	}
+
+	
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_exitLevel_button_pressed()
+	{
+		ConfirmationPopup _confirmationPopup = (ConfirmationPopup)GD.Load<PackedScene>("res://scene//ui//ConfirmationPopup.tscn").Instantiate();
+		_confirmationPopup.Init("Willst du wirklich das Level verlassen?", "Level verlassen");
+		_confirmationPopup.Confirmed += () => GetTree().Quit();
+		AddChild(_confirmationPopup);
+	}
+}
