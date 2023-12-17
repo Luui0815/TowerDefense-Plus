6c62a8c934b8e89e2909b0c494a7ee1d46045e59
R4z0rb3ard <DJIceCube09@web.de>
2023-12-17 13:18:23 +0100

    diesdas

--- script/ui/DefeatScreen.gd
+++ script/ui/DefeatScreen.gd
@@ -1,59 +1,56 @@
 extends Node
 
-# erstelle Zugriffsreferenzen auf alle Animationsplayer der 5 Figuren
-@onready var knight = $"Panel/FallenKnight/AnimationPlayer"
-@onready var archer = $"Panel/FallenArcher/AnimationPlayer"
-@onready var wizard = $"Panel/VictoriousWizard/AnimationPlayer"
-@onready var skeleton1 = $"Panel/VictoriousSkeleton1/AnimationPlayer"
-@onready var skeleton2 = $"Panel/VictoriousSkeleton2/AnimationPlayer"
+# Erstelle Zugriffsreferenzen auf alle Animationsplayer der 5 Figuren
+@onready var knight : AnimationPlayer = $"Panel/FallenKnight/AnimationPlayer"
+@onready var archer : AnimationPlayer = $"Panel/FallenArcher/AnimationPlayer"
+@onready var wizard : AnimationPlayer = $"Panel/VictoriousWizard/AnimationPlayer"
+@onready var skeleton1 : AnimationPlayer = $"Panel/VictoriousSkeleton1/AnimationPlayer"
+@onready var skeleton2 : AnimationPlayer = $"Panel/VictoriousSkeleton2/AnimationPlayer"
+@onready var timer_Knight : Timer = $"Panel/FallenKnight/TimerDeathKnight"
+@onready var timer_archer : Timer = $"Panel/FallenArcher/TimerDeathArcher"
 
-#Startwerte für Attack-Zeiten
-var time_skeleton1: float = 0
-var time_skeleton2: float = 0
+# Setze die Wartezeiten für die Timer als Konstanten
+const knight_wait_timer : float = 1.8
+const archer_wait_timer : float = 2.2
+const loop_animation: bool = true
 
-func _on_ready_animation(): #setze alle Startanimationen, wenn der Niederlagenscreen geöffnet wird
-	knight.play("AnimationsDefeatScreenKnight/KnightIdle")
-	archer.play("AnimationDefeatScreenArcher/ArcherIdle")
-	wizard.play("AnimationDefeatScreenWizard/WizardIdle")
+func _ready():
 	skeleton1.play("AnimationDefeatScreenSkeleton/Skeleton1Idle")
 	skeleton2.play("AnimationDefeatScreenSkeleton/Skeleton2Idle")
+	wizard.play("AnimationDefeatScreenWizard/WizardIdle")
+	knight.play("AnimationsDefeatScreenKnight/KnightIdle")
+	archer.play("AnimationDefeatScreenArcher/ArcherIdle")
+	skeleton1.queue("AnimationDefeatScreenSkeleton/Skeleton1Attack")
+	skeleton2.queue("AnimationDefeatScreenSkeleton/Skeleton2Attack")
+	timer_Knight.wait_time = knight_wait_timer
+	timer_Knight.one_shot = true
+	timer_Knight.autostart = true
+	timer_archer.wait_time = archer_wait_timer
+	timer_archer.one_shot = true
+	timer_archer.autostart = true
+	while not timer_Knight.timeout: 
+		knight.queue("AnimationsDefeatScreenKnight/KnightIdle")
+	while not timer_archer.timeout:
+		archer.queue("AnimationDefeatScreenArcher/ArcherGoingDown") 
+	if timer_Knight.timeout and knight.current_animation == "AnimationsDefeatScreenKnight/KnightIdle" :
+		knight.stop(false)
+		knight.play("AnimationsDefeatScreenKnight/KnightGoingDown")
+		if skeleton1.current_animation == "AnimationDefeatScreenSkeleton/Skeleton1Attack" and skeleton1.animation_finished:
+			while loop_animation == true:
+				skeleton1.queue("AnimationDefeatScreenSkeleton/Skeleton1Idle")
+				
+	if timer_archer.timeout and archer.current_animation == "AnimationsDefeatScreenArcher/ArcherIdle" :
+		archer.stop(false)
+		archer.play("AnimationsDefeatScreenArcher/ArcherGoingDown")
+		if skeleton2.current_animation == "AnimationDefeatScreenSkeleton/Skeleton2Attack" and skeleton2.animation_finished:
+			while loop_animation == true:
+				skeleton2.queue("AnimationDefeatScreenSkeleton/Skeleton2Idle")
 
-func _start_defeat_animation(): #startet Angriffsanimation der Skelete und triggert Todesanimation von Archer und Knight
-	#animation_set_next() definiert die nächste Animation, die in der Queue folgend angesteuert wird und ausgeführt
-	#Syntax animation_set_next(aktuelle Animation als String, Zielanimation als Sring)
-	skeleton1.animation_set_next("AnimationDefeatScreenSkeleton/Skeleton1Idle", "AnimationDefeatScreenSkeleton/Skeleton1Attack")
-	skeleton2.animation_set_next("AnimationDefeatScreenSkeleton/Skeleton2Idle", "AnimationDefeatScreenSkeleton/Skeleton2Attack")
-	# Wenn du Animation nicht aus Idle rausgeht von Knight und Archer sowie die Skelete nur eine Atackk starten und dann freezen muss hier eine schleife rein
-	# Diese Schleife muss solange Laufen, bis ein mal die Attack Animation durchgelaufen ist
-	# also sowas z.B. wie: while not skeleton1.animation_finished() (Arbeitsblock) do
-	# kann kann die if Abfrage mit der derzeitigen Animationszeit erfolgreich abgefangen werden
-	
-	#Vergleich ob aktuelle Animation die Attack-Animation ist für Zeitvergleich
-	if skeleton1.current_animation("AnimationDefeatScreenSkeleton/Skeleton1Attack"):
-		# Zuweisung der aktuellen Animationssequenzzeit in float Variable über curren_animation_position
-		time_skeleton1 = skeleton1.current_animation_position
-		if time_skeleton1 == 0.7:
-			# Stop und Zurücksetzen der aktuellen Animation mit stop(flase)
-			knight.stop(false)
-			knight.play("AnimationsDefeatScreenKnight/KnightGoingDown")
-			#Bafrage ob Attack Animation zu ende mit animation_finished()
-			if skeleton1.animation_finished():
-				skeleton1.play("AnimationDefeatScreenSkeleton/Skeleton1Idle")
-	# Wenn du Animation nicht aus Idle rausgeht von Knight und Archer sowie die Skelete nur eine Atackk starten und dann freezen muss hier eine schleife rein
-	# Diese Schleife muss solange Laufen, bis ein mal die Attack Animation durchgelaufen ist
-	# also sowas z.B. wie: while not skeleton1.animation_finished() (Arbeitsblock) do
-	# kann kann die if Abfrage mit der derzeitigen Animationszeit erfolgreich abgefangen werden
-	if skeleton2.current_animation("AnimationDefeatScreenSkeleton/Skeleton2Attack"):
-		time_skeleton2 = skeleton2.current_animation_length
-		if time_skeleton2 == 0.9:
-			archer.stop(false)
-			archer.play("AnimationDefeatScreenArcher/ArcherGoingDown")
-			if skeleton2.animation_finished():
-				skeleton2.play("AnimationDefeatScreenSkeleton/Skeleton2Idle")
 
-func _ready(): # Ist wie Main-Teil von C# Programm. Ab hier läuft das gesamte Script
-	_on_ready_animation()
-	_start_defeat_animation()
 
+# Callback-Funktion für den Tastendruck
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scene/ui/MainMenu.tscn")
+	
+	
+
