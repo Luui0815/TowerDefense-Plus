8f8c463ae64781b905aff6e53d7ccfd6acbd4c54
Luui0815 <147992866+Luui0815@users.noreply.github.com>
2024-01-18 19:34:02 +0100

    fix UTF 8 Code

--- script/ui/GameTutorial.cs
+++ script/ui/GameTutorial.cs
@@ -4,13 +4,13 @@ using System;
 public partial class GameTutorial : Control
 {
     private string[] _tutorialText =
-        {"Willkommen beim BA Tower Defense Game!\nDiese kleine Einfuehrung soll dem Spieler die grundlegenden Prinzipien, Inhalte und Ziele des Spiels vermitteln.",
-         "Tower Defense ist ein Genre von Strategiespielen, in welchem der Spieler eine Verteidigungslinie aufbauen muss, um Wellen von ankommenden Feinden abzuwehren. Die verteidigenden Einheiten werden dabei Tuerme (englisch: towers) genannt.",
-         "In diesem Spiel kann der Spieler zwischen mehreren Leveln auswaehlen. Sie unterscheiden sich primaer in ihrem Schwierigkeitsgrad. Ziel eines Levels ist, alle Gegnerwellen zu ueberstehen, ohne dass eine gegnerische Einheit das Schloss auf der linken Seite des Bildschirms erreicht.",
-         "Wurde ein Level ausgewaehlt, hat der Spieler eine Auswahl von verschiedenen Einheiten, welche er im Level verwenden kann. Als naechstes werden die wichtigsten Konzepte und Einheitentypen des Spiels erklaert.",
-         "1. Geld generieren\nEinheiten in einem Level zu platzieren, kostet je nach Einheit eine Summe an Geld. Diese muss der Spieler primaer durch das Platzieren von Goldminen verdienen. Diese generieren in einem bestimmten Intervall Geld.",
+        {"Willkommen beim BA Tower Defense Game!\nDiese kleine Einführung soll dem Spieler die grundlegenden Prinzipien, Inhalte und Ziele des Spiels vermitteln.",
+         "Tower Defense ist ein Genre von Strategiespielen, in welchem der Spieler eine Verteidigungslinie aufbauen muss, um Wellen von ankommenden Feinden abzuwehren. Die verteidigenden Einheiten werden dabei Türme (englisch: towers) genannt.",
+         "In diesem Spiel kann der Spieler zwischen mehreren Leveln auswählen. Sie unterscheiden sich primaer in ihrem Schwierigkeitsgrad. Ziel eines Levels ist, alle Gegnerwellen zu überstehen, ohne dass eine gegnerische Einheit das Schloss auf der linken Seite des Bildschirms erreicht.",
+         "Wurde ein Level ausgewählt, hat der Spieler eine Auswahl von verschiedenen Einheiten, welche er im Level verwenden kann. Als nächstes werden die wichtigsten Konzepte und Einheitentypen des Spiels erklärt.",
+         "1. Geld generieren\nEinheiten in einem Level zu platzieren, kostet je nach Einheit eine Summe an Geld. Diese muss der Spieler primär durch das Platzieren von Goldminen verdienen. Diese generieren in einem bestimmten Intervall Geld.",
          "Jedoch lassen auch besiegte Gegner eine kleinere Menge Geld fallen.\n4-5 Goldminen in einem Level zu platzieren wird empfohlen.",
-         "2. Verteidiger platzieren und Gegner abwehren\n Man kann zwischen zwei Kategorien von Verteidigern unterscheiden: Aktive Verteidiger und passive Verteidiger. Aktive Verteidiger greifen aktiv Feinde mit Geschossen oder im Nahkampf an, wenn diese sich in Reichweite befinden. Passive Verteidiger koennen Gegner aufhalten oder verlangsamen und die aktiven Verteidiger unterstuetzen.",
+         "2. Verteidiger platzieren und Gegner abwehren\n Man kann zwischen zwei Kategorien von Verteidigern unterscheiden: Aktive Verteidiger und passive Verteidiger. Aktive Verteidiger greifen aktiv Feinde mit Geschossen oder im Nahkampf an, wenn diese sich in Reichweite befinden. Passive Verteidiger können Gegner aufhalten oder verlangsamen und die aktiven Verteidiger unterstützen.",
          "" };
 
     private int _tutorialTextIndex = 0;
