25e38f205f4e25ddcddd133e0c98209aabe214e9
JavaRacing <javaracing1@gmail.com>
2024-01-22 09:52:11 +0100

    Add dynamic level button creation

--- script/ui/LevelSelectionMenu.cs
+++ script/ui/LevelSelectionMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using TowerDefense;
 
@@ -15,6 +16,46 @@ public partial class LevelSelectionMenu : Node
     public override void _Ready()
     {
         _playerData = GetNode<PlayerData>("/root/PlayerData");
+		CreateLevelButtons();
+    }
+
+	private void CreateLevelButtons()
+	{
+		Container levelButtonContainer = GetNode<Container>("Panel/LevelButtonContainer");
+		StyleBoxTexture normalButtonStyleBox = CreateButtonStyleBox("Stonebutton1.png");
+		StyleBoxTexture pressedButtonStyleBox = CreateButtonStyleBox("StonebuttonPressed.png");
+
+		Font buttonFont = GD.Load<Font>("res://assets/font/HyliaMidevilFont.otf");
+        Theme theme = new()
+        {
+            DefaultFont = buttonFont
+        };
+
+        foreach (Level level in Enum.GetValues(typeof(Level))) 
+		{
+            Button button = new()
+            {
+				CustomMinimumSize = new Vector2(180, 70),
+				Text = $"LEVEL {(int)level}",
+				Theme = theme
+            };
+            button.AddThemeStyleboxOverride("normal", normalButtonStyleBox);
+			button.AddThemeStyleboxOverride("pressed", pressedButtonStyleBox);
+			button.Pressed += () => OnLevelButtonPressed(level);
+			levelButtonContainer.AddChild(button);
+		}
+	}
+
+	private StyleBoxTexture CreateButtonStyleBox(string textureName)
+	{
+		Texture2D buttonTexture = GD.Load<Texture2D>($"res://assets/button/{textureName}");
+        StyleBoxTexture styleBoxTexture = new()
+        {
+            Texture = buttonTexture,
+            ExpandMarginLeft = 7,
+            ExpandMarginRight = 7
+        };
+		return styleBoxTexture;
     }
 
     public void OnSettingsButtonPressed()
@@ -28,15 +69,9 @@ public partial class LevelSelectionMenu : Node
 		GetTree().ChangeSceneToFile("res://scene/ui/MainMenu.tscn");
 	}
 
-	public void OnSelectLevel1ButtonPressed()
+	public void OnLevelButtonPressed(Level level)
 	{
-		_playerData.CurrentLevel = Level.One;
-		GetTree().ChangeSceneToFile("res://scene/ui/TowerSelectionMenu.tscn");
-	}
-
-	public void OnSelectLevel2ButtonPressed()
-	{
-		_playerData.CurrentLevel = Level.Two;
+		_playerData.CurrentLevel = level;
 		GetTree().ChangeSceneToFile("res://scene/ui/TowerSelectionMenu.tscn");
 	}
 }
